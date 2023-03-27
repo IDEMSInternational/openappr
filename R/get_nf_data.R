d101ba94364d52b7ff0d9e6ca95ac5c48de67f17
lilyclements <lily@idems.international>
2023-03-27 17:57:43 +0100

    minor amendments

--- R/get_nf_data.R
+++ R/get_nf_data.R
@@ -9,7 +9,6 @@
 get_nf_data <- function(site){
   df <- get_postgres_data(site = site, name = "app_notification_interaction")
   appdata_df <- list()
-  
   for (i in 1:nrow(df)) {
     if (!is.na(df$notification_meta[i])){
       appdata_df[[i]] <- data.frame(jsonlite::fromJSON(df$notification_meta[i]))
@@ -19,9 +18,7 @@ get_nf_data <- function(site){
   }
   # combine the list into a data frame 
   appdata <- plyr::ldply(appdata_df)
-  
   # bind into main data frame
   plhdata <- dplyr::bind_cols(df, appdata) %>% dplyr::select(-c("i"))
-  
   return(plhdata)
 }

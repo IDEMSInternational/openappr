a4ea4f8d26d97c78b745713c0f97f6eb81e3cc2a
lilyclements <lily@idems.international>
2023-04-18 18:39:07 +0100

    Increasing function efficiency

--- R/get_nf_data.R
+++ R/get_nf_data.R
@@ -1,38 +1,39 @@
-#' Get notification data from Postgres site
+#' Retrieve data from the app_notification_interaction table
 #'
-#' @param site Connection to database to get original data (using DBI::dbConnect).
-#' @param UIC_Tracker If `include_UIC_data = TRUE`, UIC tracker data. Required if `filter == TRUE`.
-#' @param app_user_id Default "app_user_id". If `include_UIC_data = TRUE`, variable in the postgres data to merge the UIC data by.
-#' @param filter logical. Default `FALSE`. Whether to filter UIC tracker data by country and study.
-#' @param country character. Only if `filter == TRUE`. The country to filter the UIC tracker data to.
-#' @param study character. Only if `filter == TRUE`. The study to filter the UIC tracker data to.
-#'
-#' @return Notification data from Postgres
+#' This function retrieves data from the `app_notification_interaction`` table in a PostgreSQL database.
+#' 
+#' @param site The name of the PostgreSQL database (using DBI::dbConnect).
+#' @param filter A logical value indicating whether to filter the UIC data frame (defaults to `FALSE`).
+#' @param UIC_Tracker A data frame containing UIC data (if filter is `TRUE`). 
+#' @param app_user_id A character string representing the name of the column containing app user IDs in `UIC_Tracker`.
+#' @param country A character string representing the country for which to retrieve data (required if filter is `TRUE`).
+#' @param study A character string representing the study for which to retrieve data (required if filter is `TRUE`).
+#' 
+#' @return Data frame containing notification data from postgres
 #' @export
-#'
+#' 
 #' @examples #TODO
-get_nf_data <- function(site, UIC_Tracker = NULL, filter = FALSE,
-                        app_user_id = "app_user_id",
-                        country = "Tanzania", study) {
+get_nf_data <- function(site, filter = FALSE, UIC_Tracker = NULL,
+                         app_user_id = "app_user_id", country = "Tanzania", study) {
   if (filter){
-    app_id <- (UIC_Tracker %>% dplyr::filter(Country == country) %>% dplyr::filter(Study == study))$YourParentAppCode
-    qry <- sprintf(paste0("select * from app_notification_interaction where ", app_user_id, " in (%s)"),
-                   paste0("'", rep(app_id), "'", collapse=","))
+    app_id <- UIC_Tracker %>% 
+      dplyr::filter(Country == country, Study == study) %>% 
+      dplyr::pull(YourParentAppCode)
+    qry <- stringr::str_c("select * from app_notification_interaction where ", app_user_id, " in ('", paste0(app_id, collapse="', '"), "')")
     df <- get_postgres_data(site = site, qry = qry)
   } else {
     df <- get_postgres_data(site = site, name = "app_notification_interaction")
   }
   appdata_df <- list()
   for (i in 1:nrow(df)) {
-    if (!is.na(df$notification_meta[i])){
+    if (!is.na(df$notification_meta[i])) {
       appdata_df[[i]] <- data.frame(jsonlite::fromJSON(df$notification_meta[i]))
-    } else {
+    }
+    else {
       appdata_df[[i]] <- data.frame(i)
     }
   }
-  # combine the list into a data frame 
-  appdata <- plyr::ldply(appdata_df)
-  # bind into main data frame
-  plhdata <- dplyr::bind_cols(df, appdata) %>% dplyr::select(-c("i"))
+  appdata <- dplyr::bind_rows(appdata_df)
+  plhdata <- dplyr::bind_cols(df, appdata)
   return(plhdata)
 }
--- R/get_user_data.R
+++ R/get_user_data.R
@@ -1,71 +1,69 @@
 #' Get user data from postgres site
+#' 
+#' @description Retrieves data from the `app_users` table in the specified PostgreSQL database, and converts the contact_fields column to a data frame using jsonlite::fromJSON().
+#' If filter is TRUE, the function further filters the data to include only rows where the Country column matches country and the Study column matches study.
 #'
-#' @param site Connection to database to get original data (using DBI::dbConnect).
-#' @param date_from Date to read in data from.
-#' @param date_to Date to read in data to.
-#' @param format_date Character string giving a date-time format as used by \code{\link[base]{strptime}}.
-#' @param tzone_date Time zone specification to be used for the conversion, if one is required.
-#' "UTC" by default. System-specific (see \code{\link[base]{as.POSIXlt}}), but "" is the current time zone, and "GMT" is UTC (Universal Time, Coordinated).
+#' @param site The name of the PostgreSQL database (using DBI::dbConnect).
+#' @param filter A logical value indicating whether to filter data (defaults to `FALSE`).
+#' @param country A character string representing the country for which to retrieve data (required if filter is `TRUE`).
+#' @param study A character string representing the study for which to retrieve data (required if filter is `TRUE`).
+#' @param UIC_Tracker A data frame containing UIC data (if `include_UIC_data = TRUE`). 
+#' @param app_user_id A character string representing the name of the column containing app user IDs in `UIC_Tracker`.
+#' @param include_UIC_data A logical value indicating whether to include UIC data (defaults to `FALSE`).
+#' @param merge_check A logical value indicating whether to merge check data (defaults to `TRUE`).
+#' @param join_UIC A character string representing the name of the column used to join UIC_Tracker and the main data frame.
+#' @param max_dist An integer specifying the maximum distance for UIC data (defaults to 5).
+#' @param date_from An optional character string representing the date from which to retrieve data.
+#' @param date_to An optional character string representing the date to which to retrieve data.
+#' @param format_date A character string specifying the format of the date strings (defaults to "%Y-%m-%d").
+#' @param tzone_date A character string specifying the time zone for the date strings (defaults to "UTC").
+#' System-specific (see \code{\link[base]{as.POSIXlt}}), but "" is the current time zone, and "GMT" is UTC (Universal Time, Coordinated).
 #' Invalid values are most commonly treated as UTC, on some platforms with a warning.
-#' @param include_UIC_data logical. Default `TRUE`. Whether to merge UIC data.
-#' @param merge_check logical. Default `TRUE`. Whether to display the close matches when merging UIC data in.
-#' @param filter logical. Default `FALSE`. Whether to filter UIC tracker data by country and study.
-#' @param country character. Only if `filter == TRUE`. The country to filter the UIC tracker data to
-#' @param study character. Only if `filter == TRUE`. The study to filter the UIC tracker data to
-#' @param UIC_Tracker If `include_UIC_data = TRUE`, UIC tracker data. 
-#' @param app_user_id Default "app_user_id". If `include_UIC_data = TRUE`, variable in the postgres data to merge the UIC data by.
-#' @param join_UIC Default "UIC". If `include_UIC_data = TRUE`, variable to merge the UIC data by.
-#' @param max_dist Default `5`. Maximum string distance when merging UIC Tracker data.
-#'
-#' @return User data from postgres
+
+#' @return Data frame containing user data from postgres
 #' @export
 #' @importFrom utils capture.output
 #'
 #' @examples # TODO
-get_user_data <- function (site, date_from, date_to = NULL, format_date = "%Y-%m-%d", 
-                           tzone_date = "UTC", include_UIC_data = FALSE, merge_check = TRUE, filter = FALSE,
-                           # if filter TRUE - give ctry and study
-                           country = "Tanzania", study,
-                           UIC_Tracker, app_user_id = "app_user_id", join_UIC = "UIC", 
-                           max_dist = 5) {
-  app_id <- (UIC_Tracker %>% dplyr::filter(Country == country) %>% dplyr::filter(Study == study))$YourParentAppCode
-  qry <- sprintf(paste0("select * from app_users where ", app_user_id, " in (%s)"),
-                 paste0("'", rep(app_id), "'", collapse=","))
-  df <- get_postgres_data(site = site, name = "app_users", qry = qry)
-  appdata_df <- list()
-  for (i in 1:nrow(df)) {
-    appdata_df[[i]] <- data.frame(jsonlite::fromJSON(df$contact_fields[i]))
+get_user_data <- function (site, filter = FALSE, country = "Tanzania", study, UIC_Tracker, app_user_id = "app_user_id",
+                           include_UIC_data = FALSE, merge_check = TRUE, join_UIC = "UIC", max_dist = 5,
+                           date_from = NULL, date_to = NULL, format_date = "%Y-%m-%d",tzone_date = "UTC") {
+  
+  if (filter){
+    app_id <- UIC_Tracker %>% 
+      dplyr::filter(Country == country, Study == study) %>% 
+      dplyr::pull(YourParentAppCode)
+    qry <- stringr::str_c("select * from app_users where ", app_user_id, " in ('", paste0(app_id, collapse="', '"), "')")
+    df <- get_postgres_data(site = site,  name = "app_users", qry = qry)
+  } else {
+    df <- get_postgres_data(site = site, name = "app_users")
   }
-  appdata <- plyr::ldply(appdata_df)
-  plhdata <- dplyr::bind_cols(df, appdata)
-  if (include_UIC_data) { # for SA UIC data only
-    plhdata_org_fuzzy <- fuzzyjoin::stringdist_full_join(x = plhdata, 
-                                                         y = UIC_Tracker, by = c(app_user_id = join_UIC[1]), 
-                                                         max_dist = max_dist)
-    plhdata_org_fuzzy_comp <- plhdata_org_fuzzy %>% dplyr::filter(!is.na(.data[[join_UIC]])) %>% 
-      dplyr::filter(app_user_id != .data[[join_UIC]] | 
-                      is.na(app_user_id)) %>% dplyr::select(app_user_id, 
-                                                            .data[[join_UIC]])
-    if (merge_check) {
-      if (yesno::yesno2("Fuzzy matches are:\n", paste0(capture.output(plhdata_org_fuzzy_comp), 
-                                                       collapse = "\n"), "\nDo you want to merge these changes in?") == 
-          TRUE) {
-        return_data <- plhdata_org_fuzzy
-      }
-      else {
-        warning("merging in fuzzy matches:\n", paste0(capture.output(plhdata_org_fuzzy_comp %>% 
-                                                                       dplyr::filter(stats::complete.cases(app_user_id))), 
-                                                      collapse = "\n"))
-        return_data <- plhdata_org_fuzzy
-      }
-    }
-    else {
-      return_data <- dplyr::full_join(x = plhdata, y = UIC_Tracker, by = c(app_user_id = join_UIC))
-    }
-  }
-  else {
+  appdata_df <- purrr::map(df$contact_fields, jsonlite::fromJSON) %>% dplyr::bind_rows()
+  plhdata <- dplyr::bind_cols(df, appdata_df)
+  
+  if (include_UIC_data) {
+    plhdata_org_fuzzy <- fuzzyjoin::stringdist_full_join(plhdata, UIC_Tracker, 
+                                              by = c(app_user_id = join_UIC), 
+                                              max_dist = max_dist)
+    plhdata_org_fuzzy_comp <- plhdata_org_fuzzy %>% 
+      dplyr::filter(stringr::str_detect(!!rlang::sym(join_UIC), "\\w+")) %>% 
+      dplyr::select(app_user_id, !!rlang::sym(join_UIC))
+    
+    return_data <- dplyr::if_else(merge_check && 
+                             yesno::yesno2("Fuzzy matches are:\n", 
+                                           paste0(capture.output(plhdata_org_fuzzy_comp), 
+                                                  collapse = "\n"), 
+                                           "\nDo you want to merge these changes in?"), 
+                           plhdata_org_fuzzy, 
+                           {warning("merging in fuzzy matches:\n", 
+                                    paste0(capture.output(plhdata_org_fuzzy_comp %>% 
+                                                            dplyr::filter(stats::complete.cases(app_user_id))), 
+                                           collapse = "\n"))
+                             plhdata_org_fuzzy})
+  } else {
     return_data <- plhdata
   }
+  
   if (!missing(date_from)) {
     return_data <- return_data %>% dplyr::filter(as.POSIXct(date_from, 
                                                             format = format_date, tzone = tzone_date) < as.POSIXct(return_data$createdAt, 
